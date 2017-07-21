12231064ce363d97d9a783518e7f185c523983b1
ansuhdaniela <ansuh.daniela@gmail.com>
2017-07-21 12:08:24 -0400

    initial commit

---
+++ TA.py
@@ -0,0 +1,97 @@
+print("Welcome")
+nextquestion = input("Press Enter to continue...")
+print("This will be series of questions based on your input of this situation.")
+
+nextquestion2 = input("Press Enter to continue...")
+inp = str(input("Your significant other does not want to be around you any more, and it can range from anger to isolation, would you 1.try to to console with the person to see whats wrong or 2.leave and remain sad?"))
+
+while inp != "1" and inp != "2":
+    inp = str(input("Enter 1 or 2: "))
+    if inp != "1" and inp != "2":
+        print("You must type 1 or 2")
+if inp == "1":
+    inp = str(input("They decides to apologize, but he has been wrongful to you before, would you be 1. skeptical or 2. try to understand and see where everything goes?"))
+    while (inp != "1" and inp != "2"):
+        inp = input("Enter 1 or 2: ")
+        if inp != "1" and inp != "2":
+            print("You must type 1 or 2")
+    if inp == "1":
+        inp = str(input("While you are skeptical, this person asked if you would like to go on a date with them, would you go because 1. they seem finally genuine or 2. go for the food?"))
+        while inp != "1" and inp != "2":
+            inp = str(input("Enter 1 or 2: "))
+            if inp != "1" and inp != "2":
+                print("You must type 1 or 2")
+        if inp == "1":
+            inp = str(input("While going on a date with this person, you look through their phone as they goes to the bathroom. You found out the has been talking to other people. 1. Get angry and leave, or 2. Order a lot of food until his card becaomes negative?"))
+            while inp != "1" and inp != "2":
+                inp = input("Enter 1 or 2: ")
+                if inp != "1" and inp != "2":
+                    print("You must type 1 or 2")
+            if inp == "1":
+                print("If you chose one, you go! The way a person was from the beginning, they will remain the same until they decide themselves to change. Go listen to Beyonce.")
+            if inp == "2":
+                print("Yes! make sure to tell the person why you have done this before a case comes. Also since you have left the phone on, the person will realize they have messed up. There fault anyways. ")
+            if inp == "2":
+                print("You have found out he has been talking to other people all along! You are very smart for following your gut. Remember that no one is worth wasting your time. Enjoy your food, block their number and let them watch your fancy snaps.")
+    if inp == "2":
+            inp = str(input("You guys slowly get to become closer again, but he says he loves you. would you want 1. to take it slow or 2. begin to love hinm back?"))
+            while inp != "1" and inp != "2":
+                inp = input("Enter 1 or 2: ")
+                if inp != "1" and inp != "2":
+                    print("You must type 1 or 2")
+            if inp == "1":
+                    print("Your significant other has been cheat all along. Since this is the case, remember to love yourself and implement self care into your life. ")
+            if inp == "2":
+                    print("Its a good thing that you have decided to take it slow, the person has been cheating because you have decided to take it slow. This person has expose their true colors and they are definitly not worth it anymore.")
+
+
+if inp == "2":
+    inp = str(input("Okay, now would you have an 1.alone time for yourself, or 2. speak to a friend?"))
+    while inp != "1" and inp != "2":
+        inp = input("Enter 1 or 2: ")
+        if inp != "1" and inp != "2":
+            print("You must type 1 or 2")
+    if inp == "1":
+        inp = str(input("In your alone time, 1. would you binge and continue to stay home or 2. become productive? "))
+        while inp != "1" and inp != "2":
+            inp = input("Enter 1 or 2: ")
+            if inp != "1" and inp != "2":
+                    print("You must type 1 or 2")
+            if inp == "1":
+                inp = str(input("You finally realize it is time to leave th ehouse and you see couples, would you 1. feel disgusted and go home or 2. feel sad but slowly come to acceptance?"))
+                while inp != "1" and inp != "2":
+                    inp = input("Enter 1 or 2: ")
+                    if inp != "1" and inp != "2":
+                        print("You must type 1 or 2")
+                if inp == "1":
+                    inp = str(input("After staying home for a year, you decide you start implement self care do you believe its time to 1. try to love yourself or 2.move on?"))
+                    while inp != "1" and inp != "2":
+                        inp = input("Enter 1 or 2: ")
+                        if inp != "1" and inp != "2":
+                            print("You must type 1 or 2")
+                    if inp == "1":
+                        print("Yes boo. Loving yourself is very important, its the first step to life. When you love yourself, situations cannot faze or move you. Great job, you have grown as a flower from dirt.")
+                    if inp == "2":
+                        print("Yes, definitely move on, Some people are not worth your time. Even though you may think your time has been wasted, you have learned from this situation. Remember to love yourself first.")
+                if inp == "2":
+                    print("That is a speedy way to recover but do not feel sad, its great that you are practicing acceptance, now practice self care and self love. ")
+    if inp == "2":
+        inp = str(input("Now that you are productive, do you believe that 1. hurt equals art, or 2. productivity has made you grown from your situation?"))
+        while inp != "1" and inp != "2":
+            inp = input("Enter 1 or 2:")
+            if inp != "1" and inp != "2":
+                print("You must type 1 or 2")
+        if inp == "1":
+            print("Hurt definitly brings art, hopefully art heals you like Picasso stages from art to life.")
+        if inp == "2":
+            print("Productivity can make you grow from a situation, always make sure it is something you enjoy because it produces dompamin and endorphens that make you feel better. Remember to love and take care of yourself.")
+
+        inp = str(input("But the friend is rarely dependable. would you 1. Be alone to relax and reflect on yourself or 2. continue to be alone?"))
+        while inp != "1" and inp != "2":
+            inp = input("Enter 1 or 2: ")
+            if inp != "1" and inp != "2":
+                print("You must type 1 or 2")
+        if inp == "1":
+            print("Good. Its good to reflect and know yourself. Always keep yourself number one in all sitatuations. You are all you have.")
+        if inp == "2":
+            print("That is a speedy way to recover but do not feel sad, its great that you are practicing seclusion to know yourself, but practice self care and self love.")
